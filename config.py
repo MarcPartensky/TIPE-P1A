30f517b2aa677d58c886de88a4eb59fdfc114b71
Marc Partensky <marc.partensky@gmail.com>
2019-04-27 20:47:54 +0200

    implémentation de la stabilité terminé

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 #CASES=[0,1]
 
-CASE_VIDE =   -1   #Ne pas mettre 0 ou 1
-DEBUGING  = True
-INFO      = True
-
-#import sys
+CASE_VIDE=-1 #Ne pas mettre 0 ou 1
+DEBUGING=True
+import sys
 
 """
 def debug(*txt) :
@@ -20,10 +18,5 @@ def debug(*txt) :
 def debug(*txt):
     if DEBUGING:
         print("[DEBUG]:",*txt)
-
-def info(nom_fichier,*txt):
-    """ne pas oublier de donner le nom du fichier dans le quelle la fonction est appeler"""
-    if INFO:
-        print("[INFO]["+nom_fichier+"]",*txt)
 
 log=debug
