bb237099b46314838cac0ed21979d2c171b0e646
Valentin <45980985+ValentinColin@users.noreply.github.com>
2019-05-17 17:08:33 +0200

    inutile

--- converter test.py
+++
@@ -1,9 +0,0 @@
-import board
-from converter import Converter
-
-print(Converter(board.Board.estMouvementValideDansLigne)())
-#print(Converter(board.Board.est_mouvement_valide_dans_ligne)())
-
-#puissance=lambda x:2**int(math.log(x)/math.log(2))
-
-#print(Converter(puissance)())
