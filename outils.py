f5657339a1a61a2fbfc0d7adb34ebfea6db39250
Valentin <45980985+ValentinColin@users.noreply.github.com>
2019-05-30 19:21:39 +0200

    Update outils.py

--- outils.py
+++ outils.py
@@ -1,77 +1,56 @@
-import random
-from config import debug
-import config
-import time
+import random,time
 
-def intersection_(liste):#todo revoir
-    """Intersection."""
-    liste=list(set(liste))
-    return liste
 
 def linearBijection(x,ensemble_entree,ensemble_sortie):
-    """Renvoie la valeur de f(x) par la bijection de l'ensemble_entree et l'ensemble_sortie."""
+    """Renvoie la valeur de f(x) par la bijection de l'ensemble_entree
+    et l'ensemble_sortie en gardant la même distance aux bornes.
+    Exemple:    on a un segment [0,10] et on veut la valeur de x=3 dans le segment [0,100].
+                Nous obtiendrons f(x)=30"""
     min1,max1=ensemble_entree
     min2,max2=ensemble_sortie
     return (x-min1)/(max1-min1)*(max2-min2)+min2
 
-def est_superieur(l1, l2) :#doit supporter les coeff negatifs
+def est_superieur(liste1, liste2) :
     """"demander alex"""
-    """
-    base=max(l1+l2)+1
-    n1=sum(l1[i]*(base**(len(l1)-i-1)) for i in range(len(l1)))
-    n2=sum(l2[i]*(base**(len(l2)-i-1)) for i in range(len(l2)))
-    return n1>=n2
-    """
-    return l1>=l2
+    ajouter_coeff_alea(liste1,liste2)
+    return liste1>=liste2
 
-def ajouter_coeff_alea(l1,l2) :
+def ajouter_coeff_alea(liste1,liste2) :
+    """Prend un prametre deux listes.
+    ajouter_coeff_alea prend une liste au hasard parmi les deux listes, lui ajoute 1 et ajoute 0 à l'autre."""
     alea_coeff = [0, 1]
     random.shuffle(alea_coeff)
-    l1.append(alea_coeff[0])
-    l2.append(alea_coeff[1])
+    liste1.append(alea_coeff[0])
+    liste2.append(alea_coeff[1])
 
 def liste_tuple_vers_liste_liste(liste_de_tuple):
     """Transforme une liste de tuple en liste de liste"""
     return [list(elem) for elem in liste_de_tuple]
+
 def liste_liste_vers_liste_tuple(liste_liste):
     """Transforme une liste de liste en liste de tuple"""
     return [tuple(l) for l in liste_liste]
 
-def deco_debug(fonction):
-    """Décorateur de fonction qui aide au débuguage.
-    Affiche les paramètre entrant et sortant, de la fonction décoré"""
-    def new_fct(*args, **kwargs) :
-        config.debug("~{}({},{})".format(fonction.__name__, args, kwargs))
-        result=fonction(*args, **kwargs)
-        config.debug("$", result)
-        return result
-    return new_fct
-
 def intersection(*args) :
-    """fait intersection de liste
+    """intersection prend en paramatre un nombre fini de listes.
+    intersection fait l'intersection de listes python au sens des ensembles.
     exemple :
-        intersection( [1,2,3], [2,3], [5,3,4]) == [3]
-        intersection( [ (1,2), (3,4) ], [ (1,2), (2,2)] ) == [(1,2)]
+        intersection( [1,2,3], [2,3], [5,3,4]) == [3] #True
+        intersection( [ (1,2), (3,4) ], [ (1,2), (2,2)] ) == [(1,2)] #True
     """
-    def intersection2(l1, l2): # devrait s'appeler intersection2a2()
-        """On définit une fonction dans une fonction"""
+    def intersection_de_2(l1, l2):
+        """Fait l'intersection de deux listes python au sens des ensembles."""
         resultat = []
         for i in l1:
             if i in l2:
                 resultat.append(i)
-        #print("l1",l1)
-        #print("l2",l2)
-        #print("resultat:",resultat)
         return resultat
-
-    #print("args:",args)
     if len(args)==1:
         return args[0]
-
     elif len(args)==2:
-        return intersection2(args[0], args[1])
-
-    return intersection(intersection2(args[0], args[1]), *args[2:])
+        return intersection_de_2(args[0], args[1])
+#On utilise les fonctions intersection et intersection_de_2 récursivement pour faire faire l'intersection des n listes
+    return intersection(intersection_de_2(args[0], args[1]), *args[2:])
 
 
 def arrangementsConsecutifs(liste,n): # mieux expliciter ce que fait cette fonction (ajouter des exemples)
@@ -98,44 +77,13 @@ def vecteur(arrivee,depart):
     vecteur=tuple([a-d for (a,d) in zip(arrivee,depart)])
     return vecteur
 
-def pgcd(a,b):
-    """Calcul du 'Plus Grand Commun Diviseur' entre les 2 nombres entiers a et b."""
-    if b==0:
-        return a
-    else:
-        r=a%b
-        return pgcd(b,r)
-
-def obtenirLigneComplete(ligne):
-    """Renvoie la ligne dont les positions position1 et position2 sont les extrémités."""
-    p1x,p1y=position1
-    p2x,p2y=position2
-    vx,vy=p2x-p1x,p2y-p1y
-    p=pgcd(vx,vy)
-    print("p:",p)
-    if p==0: return [position1,position2]
-    vx//=p
-    vy//=p
-    debug("p1x,p1y:",p1x,p1y)
-    debug("vx,vy:",vx,vy)
-    ligne=[]
-    for i in range(p+1):
-        position=(p1x+i*vx,p1y+i*vy)
-        debug("position:",position)
-        ligne.append(position)
-    #ligne=[position1,position2]
-    return ligne
-
 def obtenirLigneReduite(ligne):
     """Renvoie les extrémités d'une ligne."""
     ligne=[ligne[0],ligne[-1]]
     return ligne
 
-
-
-
 def timer(fonction):
-    """Permet de calculer le temps d'exécution d'une fonction."""
+    """Décorateur qui permet de calculer le temps d'exécution d'une fonction."""
     def nouvelleFonction(*args,**kwargs):
         """Fonction à tester."""
         ti=time.time()
@@ -145,10 +93,3 @@ def timer(fonction):
         print("[TIMER]: La fonction "+str(fonction.__name__)+" a pris "+str(dt)+" secondes.")
         return resultat
     return nouvelleFonction
-
-if __name__=="__main__": #Permet de débugger facilement les fonctions du module outils
-    print(obtenirLigneComplete([(1,2),(5,6)]))
-    print(estRemplie([1,1,1,2,1],1))
-
-    print(intersection([1,2,3],[2,3],[5,3,4]))
-    print(intersection([(1,2),(3,4)],[(1,2),(2,2)]))
