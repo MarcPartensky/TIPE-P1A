db438832793fbfa42ccbdaa0fd3d02413bfcdaef
Valentin <45980985+ValentinColin@users.noreply.github.com>
2019-05-30 19:15:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,28 @@ from othello import Othello
 from joueur import Robot,Humain,Developpeur
 
 import ia
-import ia2
 import ias
 import bruteforce as bf
 import config as cfg
 
 
-if __name__=="__main__": #Ceci est exécuté uniquement si le fichier est exécuté directement depuis ce fichier et non depuis un autre fichier.
+if __name__=="__main__":
+#Ceci est exécuté uniquement si le fichier est exécuté directement depuis ce fichier et non depuis un autre fichier.
+#À retirer avant de passer à l'éxécutable
 
     panneau=Panneau(nom="Othello",taille=cfg.RESOLUTION_FENETRE,set=False,fullscreen=False) # Crée une fenêtre
 
     #On crée des joueurs ici
 
+    # Création des joueur humain et non naïvent
     humain1=Humain(nom="Humain1") #Crée un joueur humain.
     humain2=Humain(nom="Humain2") #Même humain
     developpeur1=Developpeur(nom="Developpeur1") #Crée un joueur humain qui n'obéit pas aux règles de l'Othello, cela permet de faire des tests
     developpeur2=Developpeur(nom="Developpeur2") #Même développeur
     machine1=ia.IA(nom="Stable1") #Crée une intelligence artificielle utilisant la notion de stabilite
     machine2=ia.IA(nom="Stable2") #Même ia
+
+    # Création des IAs naïvent
     machine3=ias.Interieur(nom="Interieur") #Joue toujours le plus proche possible du centre du plateau
     machine4=ias.Exterieur(nom="Exterieur") #Joue toujours le plus loin possible du centre du plateau
     machine5=ias.Groupe(nom="Groupe") #Joue de façon à former des groupes de pions
@@ -29,13 +33,16 @@ if __name__=="__main__": #Ceci est exécuté uniquement si le fichier est exécu
     machine8=ias.Aleatoire(nom="Aleatoire") #Joue aléatoirement
     machine9=ias.PremierCoup(nom="Premier Coup") #Joue toujours le premier coup parmi ceux proposé
     machine10=ias.Direct(nom="Direct") #Joue en essayant de maximiser son nombre de pions sur 1 tour seulement
+
+    # Création d'Une IA utilisant la puissance de calcul
     bruteforce1=bf.BruteForce(nom="Brute Force niveau 1", level=1) #Crée une machine utilisant la force de calcul de la machine, cela est utile pour les tests de niveau des nouvelles intelligences artificielles.
     bruteforce2=bf.BruteForce(nom="Brute Force niveau 2", level=2) #Joue en pensant 2 coups à l'avance
-    bruteforce3=bf.BruteForce(nom="Brute Force niveau 3", level=4)  #Joue en pensant 3 coups à l'avance
+    bruteforce3=bf.BruteForce(nom="Brute Force niveau 3", level=3) #Joue en pensant 3 coups à l'avance
+    bruteforce3=bf.BruteForce(nom="Brute Force niveau 4", level=4) #Joue en pensant 4 coups à l'avance
+
 
     #puis on  choisit les joueurs ici
-
-    joueur_blanc = machine7
+    joueur_blanc = machine1
     joueur_noir  = humain1
 
     #et non ici
